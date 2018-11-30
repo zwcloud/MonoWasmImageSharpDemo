b0d4615b7469678eac4c7775ce942927ea221d78
Zou Wei <zwcloud@hotmail.com>
2018-11-30 12:45:55 +0800

    Created a mono-wasm project.

---
+++ ImageCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from C#!");
+        }
+    }
+}
